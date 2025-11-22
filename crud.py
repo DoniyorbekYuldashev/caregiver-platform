05819e4a2900afbd649cbc6ec1f5625bbc9689f2
Doniyor Yuldashev <doniyojon.yuldashev@gmail.com>
2025-11-22 22:55:40 +0500

    Fix CRUD function signatures

--- crud.py
+++ crud.py
@@ -1,257 +1,198 @@
 # Part 3: CRUD operations
 
 from sqlalchemy.orm import Session
-from sqlalchemy import or_
-from models import User, Caregiver, Member, Address, Job, JobApplication, Appointment
-from schemas import *
+from models import User, Caregiver, Member, Job, Appointment
+from datetime import datetime
 
 
-# USER CRUD
-
-def create_user(db: Session, user: UserCreate):
-    db_user = User(**user.model_dump())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
-
-
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(User).offset(skip).limit(limit).all()
-
+def get_users(db: Session):
+    return db.query(User).all()
 
 def get_user(db: Session, user_id: int):
     return db.query(User).filter(User.user_id == user_id).first()
 
+def create_user(db: Session, email: str, given_name: str, surname: str, city: str,
+                phone_number: str, profile_description: str, password: str):
+    user = User(
+        email=email,
+        given_name=given_name,
+        surname=surname,
+        city=city,
+        phone_number=phone_number,
+        profile_description=profile_description,
+        password=password
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
 
-def update_user(db: Session, user_id: int, user: UserUpdate):
-    db_user = get_user(db, user_id)
-    if db_user:
-        for key, value in user.model_dump(exclude_unset=True).items():
-            setattr(db_user, key, value)
+def update_user(db: Session, user_id: int, email: str, given_name: str, surname: str,
+                city: str, phone_number: str, profile_description: str, password: str):
+    user = db.query(User).filter(User.user_id == user_id).first()
+    if user:
+        user.email = email
+        user.given_name = given_name
+        user.surname = surname
+        user.city = city
+        user.phone_number = phone_number
+        user.profile_description = profile_description
+        user.password = password
         db.commit()
-        db.refresh(db_user)
-    return db_user
-
+        db.refresh(user)
+    return user
 
 def delete_user(db: Session, user_id: int):
-    db_user = get_user(db, user_id)
-    if db_user:
-        db.delete(db_user)
+    user = db.query(User).filter(User.user_id == user_id).first()
+    if user:
+        db.delete(user)
         db.commit()
-        return True
-    return False
+    return user
 
 
-# CAREGIVER CRUD
-
-def create_caregiver(db: Session, caregiver: CaregiverCreate):
-    db_caregiver = Caregiver(**caregiver.model_dump())
-    db.add(db_caregiver)
-    db.commit()
-    db.refresh(db_caregiver)
-    return db_caregiver
-
-
-def get_caregivers(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(Caregiver).offset(skip).limit(limit).all()
-
+def get_caregivers(db: Session):
+    return db.query(Caregiver).all()
 
 def get_caregiver(db: Session, caregiver_id: int):
-    return db.query(Caregiver).filter(Caregiver.caregiver_user_id == caregiver_id).first()
+    return db.query(Caregiver).filter(Caregiver.caregiver_id == caregiver_id).first()
 
+def create_caregiver(db: Session, user_id: int, photo_url: str, gender: str,
+                     caregiving_type: str, hourly_rate: float):
+    caregiver = Caregiver(
+        user_id=user_id,
+        photo_url=photo_url,
+        gender=gender,
+        caregiving_type=caregiving_type,
+        hourly_rate=hourly_rate
+    )
+    db.add(caregiver)
+    db.commit()
+    db.refresh(caregiver)
+    return caregiver
 
-def update_caregiver(db: Session, caregiver_id: int, caregiver: CaregiverUpdate):
-    db_caregiver = get_caregiver(db, caregiver_id)
-    if db_caregiver:
-        for key, value in caregiver.model_dump(exclude_unset=True).items():
-            setattr(db_caregiver, key, value)
+def update_caregiver(db: Session, caregiver_id: int, photo_url: str, gender: str,
+                     caregiving_type: str, hourly_rate: float):
+    caregiver = db.query(Caregiver).filter(Caregiver.caregiver_id == caregiver_id).first()
+    if caregiver:
+        caregiver.photo_url = photo_url
+        caregiver.gender = gender
+        caregiver.caregiving_type = caregiving_type
+        caregiver.hourly_rate = hourly_rate
         db.commit()
-        db.refresh(db_caregiver)
-    return db_caregiver
-
+        db.refresh(caregiver)
+    return caregiver
 
 def delete_caregiver(db: Session, caregiver_id: int):
-    """Delete caregiver with all related records"""
-    try:
-        # Delete related job applications first
-        db.query(JobApplication).filter(
-            JobApplication.caregiver_user_id == caregiver_id
-        ).delete(synchronize_session=False)
-
-        # Delete related appointments
-        db.query(Appointment).filter(
-            Appointment.caregiver_user_id == caregiver_id
-        ).delete(synchronize_session=False)
-
-        # Now delete the caregiver
-        db_caregiver = get_caregiver(db, caregiver_id)
-        if db_caregiver:
-            db.delete(db_caregiver)
-            db.commit()
-            return True
-        return False
-    except Exception as e:
-        db.rollback()
-        raise e
+    caregiver = db.query(Caregiver).filter(Caregiver.caregiver_id == caregiver_id).first()
+    if caregiver:
+        db.delete(caregiver)
+        db.commit()
+    return caregiver
 
 
-# MEMBER CRUD
-
-def create_member(db: Session, member: MemberCreate):
-    db_member = Member(**member.model_dump())
-    db.add(db_member)
-    db.commit()
-    db.refresh(db_member)
-    return db_member
-
-
-def get_members(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(Member).offset(skip).limit(limit).all()
-
+def get_members(db: Session):
+    return db.query(Member).all()
 
 def get_member(db: Session, member_id: int):
-    return db.query(Member).filter(Member.member_user_id == member_id).first()
+    return db.query(Member).filter(Member.member_id == member_id).first()
 
+def create_member(db: Session, user_id: int, house_rules: str):
+    member = Member(
+        user_id=user_id,
+        house_rules=house_rules
+    )
+    db.add(member)
+    db.commit()
+    db.refresh(member)
+    return member
 
-def update_member(db: Session, member_id: int, member: MemberUpdate):
-    db_member = get_member(db, member_id)
-    if db_member:
-        for key, value in member.model_dump(exclude_unset=True).items():
-            setattr(db_member, key, value)
+def update_member(db: Session, member_id: int, house_rules: str):
+    member = db.query(Member).filter(Member.member_id == member_id).first()
+    if member:
+        member.house_rules = house_rules
         db.commit()
-        db.refresh(db_member)
-    return db_member
-
+        db.refresh(member)
+    return member
 
 def delete_member(db: Session, member_id: int):
-    """Delete member with all related records"""
-    try:
-        # Delete related appointments
-        db.query(Appointment).filter(
-            Appointment.member_user_id == member_id
-        ).delete(synchronize_session=False)
-
-        # Delete related jobs (and their applications will cascade)
-        db.query(Job).filter(
-            Job.member_user_id == member_id
-        ).delete(synchronize_session=False)
-
-        # Delete address
-        db.query(Address).filter(
-            Address.member_user_id == member_id
-        ).delete(synchronize_session=False)
-
-        # Now delete the member
-        db_member = get_member(db, member_id)
-        if db_member:
-            db.delete(db_member)
-            db.commit()
-            return True
-        return False
-    except Exception as e:
-        db.rollback()
-        raise e
+    member = db.query(Member).filter(Member.member_id == member_id).first()
+    if member:
+        db.delete(member)
+        db.commit()
+    return member
 
 
-# JOB CRUD
-
-def create_job(db: Session, job: JobCreate):
-    from datetime import datetime
-    db_job = Job(**job.model_dump(), date_posted=datetime.now())
-    db.add(db_job)
-    db.commit()
-    db.refresh(db_job)
-    return db_job
-
-
-def get_jobs(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(Job).offset(skip).limit(limit).all()
-
+def get_jobs(db: Session):
+    return db.query(Job).all()
 
 def get_job(db: Session, job_id: int):
     return db.query(Job).filter(Job.job_id == job_id).first()
 
+def create_job(db: Session, member_id: int, required_caregiving_type: str, other_requirements: str):
+    job = Job(
+        member_id=member_id,
+        required_caregiving_type=required_caregiving_type,
+        other_requirements=other_requirements
+    )
+    db.add(job)
+    db.commit()
+    db.refresh(job)
+    return job
 
-def update_job(db: Session, job_id: int, job: JobUpdate):
-    db_job = get_job(db, job_id)
-    if db_job:
-        for key, value in job.model_dump(exclude_unset=True).items():
-            setattr(db_job, key, value)
+def update_job(db: Session, job_id: int, required_caregiving_type: str, other_requirements: str):
+    job = db.query(Job).filter(Job.job_id == job_id).first()
+    if job:
+        job.required_caregiving_type = required_caregiving_type
+        job.other_requirements = other_requirements
         db.commit()
-        db.refresh(db_job)
-    return db_job
-
+        db.refresh(job)
+    return job
 
 def delete_job(db: Session, job_id: int):
-    """Delete job with all related applications"""
-    try:
-        # Delete related job applications
-        db.query(JobApplication).filter(
-            JobApplication.job_id == job_id
-        ).delete(synchronize_session=False)
-
-        # Now delete the job
-        db_job = get_job(db, job_id)
-        if db_job:
-            db.delete(db_job)
-            db.commit()
-            return True
-        return False
-    except Exception as e:
-        db.rollback()
-        raise e
+    job = db.query(Job).filter(Job.job_id == job_id).first()
+    if job:
+        db.delete(job)
+        db.commit()
+    return job
 
 
-# APPOINTMENT CRUD
-
-def create_appointment(db: Session, appointment: AppointmentCreate):
-    db_appointment = Appointment(**appointment.model_dump())
-    db.add(db_appointment)
-    db.commit()
-    db.refresh(db_appointment)
-    return db_appointment
-
-
-def get_appointments(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(Appointment).offset(skip).limit(limit).all()
-
+def get_appointments(db: Session):
+    return db.query(Appointment).all()
 
 def get_appointment(db: Session, appointment_id: int):
     return db.query(Appointment).filter(Appointment.appointment_id == appointment_id).first()
 
+def create_appointment(db: Session, caregiver_id: int, member_id: int,
+                       appointment_date: str, appointment_time: str,
+                       work_hours: float, status: str):
+    appointment = Appointment(
+        caregiver_id=caregiver_id,
+        member_id=member_id,
+        appointment_date=datetime.strptime(appointment_date, '%Y-%m-%d').date(),
+        appointment_time=appointment_time,
+        work_hours=work_hours,
+        status=status
+    )
+    db.add(appointment)
+    db.commit()
+    db.refresh(appointment)
+    return appointment
 
-def update_appointment(db: Session, appointment_id: int, appointment: AppointmentUpdate):
-    db_appointment = get_appointment(db, appointment_id)
-    if db_appointment:
-        for key, value in appointment.model_dump(exclude_unset=True).items():
-            setattr(db_appointment, key, value)
+def update_appointment(db: Session, appointment_id: int, appointment_date: str,
+                       appointment_time: str, work_hours: float, status: str):
+    appointment = db.query(Appointment).filter(Appointment.appointment_id == appointment_id).first()
+    if appointment:
+        appointment.appointment_date = datetime.strptime(appointment_date, '%Y-%m-%d').date()
+        appointment.appointment_time = appointment_time
+        appointment.work_hours = work_hours
+        appointment.status = status
         db.commit()
-        db.refresh(db_appointment)
-    return db_appointment
-
+        db.refresh(appointment)
+    return appointment
 
 def delete_appointment(db: Session, appointment_id: int):
-    db_appointment = get_appointment(db, appointment_id)
-    if db_appointment:
-        db.delete(db_appointment)
+    appointment = db.query(Appointment).filter(Appointment.appointment_id == appointment_id).first()
+    if appointment:
+        db.delete(appointment)
         db.commit()
-        return True
-    return False
-
-
-# SEARCH & FILTER
-
-def search_caregivers(db: Session, caregiving_type: str = None, city: str = None):
-    query = db.query(Caregiver).join(User)
-    if caregiving_type:
-        query = query.filter(Caregiver.caregiving_type == caregiving_type)
-    if city:
-        query = query.filter(User.city == city)
-    return query.all()
-
-
-def get_accepted_appointments(db: Session):
-    return db.query(Appointment).filter(
-        or_(Appointment.status == 'confirmed', Appointment.status == 'completed')
-    ).all()
+    return appointment
